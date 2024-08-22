47255e8271784bbdb0081c40ceb7e1a43c1a5ccd
geovannad <geovanna.diniz@germinare.org.br>
2024-08-22 11:20:11 -0300

    config repo

---
+++ src/main/java/com/example/apitrocatinesql/ApiTrocaTineSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.apitrocatinesql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTrocaTineSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTrocaTineSqlApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/apitrocatinesql/models/Users.java
@@ -0,0 +1,14 @@
+package com.example.apitrocatinesql.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+class Users {
+
+}
