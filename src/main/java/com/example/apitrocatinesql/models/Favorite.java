13ee5175a64d419031e218b47f9e46f5e82734d6
geovannad <geovanna.diniz@germinare.org.br>
2024-10-19 18:02:37 -0300

    creating again favorite class

---
+++ src/main/java/com/example/apitrocatinesql/models/Favorite.java
@@ -0,0 +1,30 @@
+
+package com.example.apitrocatinesql.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Table(name = "favorites")
+public class Favorite {
+
+    @EmbeddedId
+    private FavoriteId id;
+
+    @ManyToOne
+    @MapsId("idUser")
+    @JoinColumn(name = "id_user")
+    private User user;
+
+    @ManyToOne
+    @MapsId("idProduct")
+    @JoinColumn(name = "id_product")
+    private Product product;
+
+}
